7856f88c4fa44f20d4d77e87ed97fd1ff64c1809
kevinSelles <kevin.selles.c@gmail.com>
2025-09-24 01:49:52 +0200

    .

--- back/index.js
+++ back/index.js
@@ -1,32 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const serverless = require("serverless-http");
-
 const { connectDB } = require("../src/config/db");
 const activitiesRouter = require("../src/api/routes/activities");
 const usersRouter = require("../src/api/routes/users");
 const galleryRouter = require("../src/api/routes/gallery");
 const contactRouter = require("../src/api/routes/contact");
+
 require("../src/config/cloudinary");
 
 const app = express();
 
-const FRONT_URL = process.env.FRONT_URL || "http://localhost:5173";
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (!origin || origin === FRONT_URL) {
-    res.setHeader("Access-Control-Allow-Origin", origin || FRONT_URL);
-  } else {
-    res.setHeader("Access-Control-Allow-Origin", FRONT_URL);
-  }
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") return res.status(204).end();
-  next();
-});
+const FRONT_URL = "https://splish-splash-cadiz.vercel.app" || "http://localhost:5173";
+
+app.use(cors({
+  origin: FRONT_URL,
+  credentials: true
+}));
 
 app.use(express.json());
+
 connectDB();
 
 app.use("/api/v1/activities", activitiesRouter);
@@ -34,7 +28,9 @@ app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/gallery", galleryRouter);
 app.use("/api/v1/contact", contactRouter);
 
-app.use((req, res) => res.status(404).json("Ruta no encontrada"));
+app.use((req, res, next) => {
+  return res.status(404).json("Ruta no encontrada");
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
