8e363c0489bb41b0d2e374ee78eb4161d83f8aa6
kevinSelles <kevin.selles.c@gmail.com>
2025-09-24 22:47:15 +0200

    Update contact.js

--- back/src/api/controllers/contact.js
+++ back/src/api/controllers/contact.js
@@ -8,13 +8,38 @@ const sendContact = async (req, res) => {
   }
 
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      replyTo: email,
-      to: process.env.EMAIL_USER,
-      subject: `Consulta de ${name}`,
-      text: message,
-      html: `<p>${message}</p><p>De: ${name} (${email})</p>`
+    await new Promise((resolve, reject) => {
+      transporter.verify((error, success) => {
+        if (error) {
+          console.error("Error en verificación SMTP:", error);
+          reject(error);
+        } else {
+          console.log("Servidor listo para enviar correos");
+          resolve(success);
+        }
+      });
+    });
+
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+        from: process.env.EMAIL_USER,
+        replyTo: email,
+        to:process.env.EMAIL_USER,
+        subject: `Consulta de ${name}`,
+        text: message,
+        html: `<p>${message}</p><p>De: ${name} (${email})</p>`
+        },
+        (error, info) => {
+          if (error) {
+            console.error("Error enviando correo:", error)
+            reject(error);
+          } else {
+            console.log("Correo enviado:", info.response);
+            resolve(info);
+          }
+        }
+      )
     });
 
     res.json({ message: "Correo enviado correctamente" });
